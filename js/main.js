6c2a16300527acc8a667d9fdf4e7a63cb530e11e
EugeneAbros <eugene.abros@gmail.com>
2018-10-24 11:22:50 +0300

    Adding cities function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,27 @@ function activatePlacesSearch () {
     var input = document.getElementById('searchInput');
     var autocomplete = new google.maps.places.Autocomplete(input);
 }
+
+new Vue({
+    el: '#weather-app',
+    data: {
+        cities: [
+            { name: "London" },
+            { name: "Kiev" }
+        ]
+    },
+    methods: {
+        addCity: function () {
+            var input = document.getElementById("searchInput");
+            if ( input.value !== '' ) {
+                this.cities.push ({
+                    name: input.value
+                })
+                input.value = '';
+            }
+        },
+        removeCity: function (index) {
+            this.cities.splice(index, 1);
+        }
+    }
+});
